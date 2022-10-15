a7b3a7e0b001540d04f51ec643042ccf6c248fa2
floathh <95484059+floathh@users.noreply.github.com>
2022-10-15 20:03:34 +0800

    pull commit

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix!");
         System.out.println("master test!");
         System.out.println("hot-fix合并2!");
+        System.out.println("pull test!");
     }
 }
