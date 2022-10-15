076cc8c9f9d42869349ab7dc9d755bad98efa629
Ethan <Ethan@atguigu.com>
2022-10-15 15:43:37 +0800

    third commit

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -2,10 +2,12 @@ package com.atguigu.git;
 
 /**
  * @author LiangYihan
- * @create 2022-10-15 15:15
+ * 
  */
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
     }
