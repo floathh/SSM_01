21073e2469c83b8640d52110b4c5d77bc3918c42
Ethan <Ethan@atguigu.com>
2022-10-15 15:34:43 +0800

    fi

---
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LiangYihan
+ * @create 2022-10-15 15:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
